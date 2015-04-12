574cab49e6c78bfc173dd0b593dd6b932be3c8be
Mahmoud Bouchikhi <mahmoudbouchikhi@gmail.com>
2015-04-11 21:38:34 -0600

    FizzBuzz

---
+++ WeekOne Review/WeekOne Review/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekOne_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void SpecificLetterCounter(String letterToCount,string stringTosearch)
+    {
+            //Specificlettere counter("a, Lamond");
+        {
+          int numberOfMatches=0
+        for (int i=0
+        }
+
+
+    }
+
+}
